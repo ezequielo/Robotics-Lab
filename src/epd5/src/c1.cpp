9c06e7496064fbdf3c27c4084d68c505e8be1b45
ezequiel <ezequielra1@gmail.com>
2015-05-06 22:52:13 +0200

    refactor command method & angle fix

--- src/epd5/src/c1.cpp
+++ src/epd5/src/c1.cpp
@@ -8,228 +8,181 @@
 #include <math.h>
 #include <stdio.h>
 
-
 /**
-* Our class to control the robot
-* It has members to store the robot pose, and
-* methods to control the robot by publishing data
-*/
-class Turtlebot
-{
+ * Our class to control the robot
+ * It has members to store the robot pose, and
+ * methods to control the robot by publishing data
+ */
+class Turtlebot {
 public:
-  Turtlebot();
- 
+    Turtlebot();
 
-  /**
-   * This function should command the robot to reach the goal
-   * It should compute the commands to the robot by knowing the current position
-   * and the goal position
-   */
-  void command(double goal_x, double goal_y);
+    /**
+     * This function should command the robot to reach the goal
+     * It should compute the commands to the robot by knowing the current position
+     * and the goal position
+     */
+    void command(double goal_x, double goal_y);
 
 private:
 
-  
-  ros::NodeHandle nh_;
-  
-  //2D robot pose
-  double x,y,theta;
+    ros::NodeHandle nh_;
 
-  //Publisher and subscribers
-  ros::Publisher vel_pub_;
-  ros::Subscriber pose_sub_;
+    //2D robot pose
+    double x, y, theta;
 
-  //!Publish the command to the turtlebot
-  void publish(double angular_vel, double linear_vel);
+    //Publisher and subscribers
+    ros::Publisher vel_pub_;
+    ros::Subscriber pose_sub_;
 
-  //!Callback for robot position
-  void receivePose(const nav_msgs::OdometryConstPtr & pose);
+    // helper methods
+    double angleDiff(double dest_angle);
+    double computeW(double dest_angle);
+    double computeV(double gx, double gy);
+    
+    //!Publish the command to the turtlebot
+    void publish(double angular_vel, double linear_vel);
+
+    //!Callback for robot position
+    void receivePose(const nav_msgs::OdometryConstPtr & pose);
 
 };
 
-Turtlebot::Turtlebot()
-{
-  vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
-
-  pose_sub_ = nh_.subscribe("/odom",1,&Turtlebot::receivePose,this);
-
-}
-
-
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "robot_control");
-  Turtlebot robot;
-  ros::NodeHandle n;
-
-  if(argc<3)
-  {
-	std::cout << "Insuficient number of parameters" << std::endl;
-	std::cout << "Usage: c1 Goal_coordinate_x Goal_coordinate_y" << std::endl;
-	return 0;
-  }
-
-  double xGoal=5.0, yGoal=3.0;
-
-  xGoal=std::atof(argv[1]);
-  yGoal=std::atof(argv[2]);
-
-  ros::Rate loop_rate(20);
-
-  while (ros::ok())
-  {
-   
-    ros::spinOnce();
-
-    robot.command(xGoal,yGoal);
-
-    loop_rate.sleep();
-   
-  }
-
-
-  return 0;
+Turtlebot::Turtlebot() {
+    
+    vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1);
+    pose_sub_ = nh_.subscribe("/odom", 1, &Turtlebot::receivePose, this);
 
 }
 
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "robot_control");
+    Turtlebot robot;
+    ros::NodeHandle n;
 
-void Turtlebot::command(double gx, double gy)  
-{
+    if (argc < 3) {
+        std::cout << "Insuficient number of parameters" << std::endl;
+        std::cout << "Usage: c1 Goal_coordinate_x Goal_coordinate_y" << std::endl;
+        return 0;
+    }
 
-	double linear_vel=0.0;
-	double angular_vel=0.0;
+    xGoal = std::atof(argv[1]);
+    yGoal = std::atof(argv[2]);
 
-	double margen = 0.05;
-	double v_base = 0.1;
-	
-	std::cout << "Dif X: " << gx << " - " << x << " = " << gx-x << std::endl;
-	std::cout << "Dif Y: " << gy << " - " << y << " = " << gy-y << std::endl;
+    ros::Rate loop_rate(20);
+
+    while (ros::ok()) {
+
+        ros::spinOnce();
+        robot.command(xGoal, yGoal);
+        loop_rate.sleep();
         
-	if (fabs(gx-x)<margen && fabs(gy-y)<margen){
-		
-		std::cout << "Hemos llegado! " << std::endl;
+    }
 
-		linear_vel = 0.0;
-		angular_vel = 0.0;
+    return 0;
+}
 
-	} else {
 
-		// calcular angulo
-		
-		double angulo = atan2((gy-y),(gx-x));
-        
-        if ( angulo < 0) {
-            // transform angulo
-            angulo = (2 * M_PI) + angulo;
-        }
-        
-        if (theta < -0.1) {
-            // transform theta
-            theta = (2 * M_PI) + theta;
-        }
-        
-        std::cout << "Tan: " << tan << std::endl;
+
+double Turtlebot::angleDiff(double dest_angle){
+    
+    double diff = fmod(dest_angle - theta + 180, 360) - 180;
+    return diff < -180 ? diff + 360 : diff;
+    
+}
+
+
+double Turtlebot::computeW(double dest_angle) {
+
+    double diff = angleDiff(dest_angle);
+    double base_w = 0.1;
+    double w = base_w * diff < base_w ? base_w : base_w * diff;
+    return diff >= 0 ? w : -w;
+    
+}
+
+
+double Turtlebot::computeV(double gx, double gy){
+    
+    // distancia euclidea
+    double dx = gx - x;
+    double dy = gy - y;
+    double dist = sqrt((dx * dx) + (dy * dy));
+
+    // calculo de la velocidad
+    double v_base = 0.1;
+    return v_base * dist > v_base ? v_base * dist : v_base;
+    
+}
+
+
+void Turtlebot::command(double gx, double gy) {
+
+    double linear_vel;
+    double angular_vel;
+    double margen = 0.05;
+
+    std::cout << "Dif X: " << gx << " - " << x << " = " << gx - x << std::endl;
+    std::cout << "Dif Y: " << gy << " - " << y << " = " << gy - y << std::endl;
+
+    if (fabs(gx - x) < margen && fabs(gy - y) < margen) {
+
+        linear_vel = 0.0;
+        angular_vel = 0.0;
+        std::cout << "Hemos llegado! " << std::endl;
+
+    } else {
+
+        // calcular angulo
+        double angulo = atan2((gy - y), (gx - x));
         std::cout << "Theta: " << theta << std::endl;
         std::cout << "Angulo: " << angulo << " Margen:  " << margen << std::endl;
-		
-		if (fabs(angulo-theta) < margen){			
-			
-			// desplazamiento mejorado
-			// la velocidad depende de la distancia al goal
 
-			
-			// distancia euclidea
-
-			double dx = gx - x;
-			double dy = gy - y;
-			double dist = sqrt((dx*dx) + (dy*dy));
-
-			// calculo de la velocidad
-
-			if (v_base * dist > v_base) {
-
-				linear_vel = v_base * dist;
- 
-			} else {
-
-				linear_vel = v_base;
-
-			}
+        if (fabs(angulo - theta) < margen) {
             
+            // calculo de V
+            linear_vel = computeV(gx, gy);
             // corrección del angualo sobre la marcha
+            angular_vel = computeW(angulo);
+            std::cout << "Desplazando a  " << linear_vel << " m/s Girando a " << angular_vel << " rad/s" << std::endl;
+
+        } else {
             
-            if ((angulo-theta) > 0 ) {
-                
-                angular_vel = 0.05;
-                std::cout << "Rotando IZQ sobre la marcha! " << std::endl;
-                
-            } else {
-                
-                angular_vel = -0.05;
-                std::cout << "Rotando DRA sobre la marcha! " << std::endl;
-                
-            }
-			
-			std::cout << "Distancia al objetivo: " << dist << std::endl;
-			std::cout << "Desplazando a  " << linear_vel << " m/s" << std::endl;
+            // calculo de W
+            angular_vel = computeW(angulo);
+            std::cout << "Girando a " << angular_vel << " rad/s" << std::endl;
+            
+        }
+    }
 
-		} else {
-
-			// rotacion mejorada
-			// gira en sentido del angulo menor
-
-
-			if ((angulo-theta) > 0 ) {
-
-				angular_vel = 0.1;
-				std::cout << "Rotando IZQ! " << std::endl;
-
-			} else {
-
-				angular_vel = -0.1;
-				std::cout << "Rotando DRA! " << std::endl;
-
-			}
-			
-		}
-	}
-
-   	publish(angular_vel,linear_vel);    
-
-
-  return;
+    publish(angular_vel, linear_vel);
+    return;
 }
 
 
 
 //Publish the command to the turtlebot
-void Turtlebot::publish(double angular, double linear)  
-{
+
+void Turtlebot::publish(double angular, double linear) {
     geometry_msgs::Twist vel;
     vel.angular.z = angular;
     vel.linear.x = linear;
 
-    vel_pub_.publish(vel);    
-
-
-  return;
+    vel_pub_.publish(vel);
+    return;
 }
 
 
 //Callback for robot position
-void Turtlebot::receivePose(const nav_msgs::OdometryConstPtr& msg)
-{
 
-	x=msg->pose.pose.position.x;
-	y=msg->pose.pose.position.y;
+void Turtlebot::receivePose(const nav_msgs::OdometryConstPtr& msg) {
 
-	theta=tf::getYaw(msg->pose.pose.orientation);
+    x = msg->pose.pose.position.x;
+    y = msg->pose.pose.position.y;
 
-	std::cout << "Pose: " << x << ", " << y << " ," << theta << std::endl;
-        std::cout << "-----------------------------------" << std::endl;
+    theta = tf::getYaw(msg->pose.pose.orientation);
 
-
+    std::cout << "Pose: " << x << ", " << y << " ," << theta << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    
 }
-
-
-
