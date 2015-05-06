36b6bb407f543b4e704fc6afcdacf714b294acc4
ezequielo <ezequielra1@gmail.com>
2015-05-06 20:41:38 +0200

    Simple obstacle avoidance: left side fix

--- src/epd6/src/c5.cpp
+++ src/epd6/src/c5.cpp
@@ -42,7 +42,7 @@ public:
      */
     bool command(double goal_x, double goal_y);
     bool isPathClear();
-    float findAltGoal();
+    std::vector<float> findAltGoal();
 
 private:
 
@@ -58,6 +58,10 @@ private:
     ros::Publisher vel_pub_;
     ros::Subscriber kinect_sub_;
     ros::Subscriber pose_sub_;
+
+
+    bool isClear(int k);
+    int findClearRegion(int k);
 
     //!Publish the command to the turtlebot
     void publish(double angular_vel, double linear_vel);
@@ -80,43 +84,259 @@ Turtlebot::Turtlebot() {
 
 }
 
-
 /*
-    command 
+    command
  */
 
 bool Turtlebot::command(double gx, double gy) {
 
-    double linear_vel = 0.0;
-    double angular_vel = 0.0;
-    double dist_x, dist_y, distance, prod;
-    double angulo;
     bool ret_val = false;
 
+    double linear_vel = 0.0;
+    double angular_vel = 0.0;
 
-    linear_vel = 0.1;
-    
-    publish(angular_vel,linear_vel);    
+    double margen = 0.05;
+    double v_base = 0.1;
 
-    receiveKinect(data_scan);
+    std::cout << "Dif X: " << gx << " - " << x << " = " << gx - x << std::endl;
+    std::cout << "Dif Y: " << gy << " - " << y << " = " << gy - y << std::endl;
+
+    if (fabs(gx - x) < margen && fabs(gy - y) < margen) {
+
+        std::cout << "Hemos llegado! " << std::endl;
+
+        linear_vel = 0.0;
+        angular_vel = 0.0;
+        ret_val = true;
+
+    } else {
+
+        // calcular angulo
+
+        double angulo = atan2((gy-y),(gx-x));
+
+        std::cout << "Angulo ANTES: " << angulo << " rads // " << angulo * (180 / M_PI) << " grados" << std::endl;
+        std::cout << "Theta ANTES: " << theta << " rads // " << theta * (180 / M_PI) << " grados" << std::endl;
+
+
+        if (angulo < 0) {
+            // transform angulo
+            angulo = (2 * M_PI) + angulo;
+        }
+
+        if (theta < -0.1) {
+            // transform theta
+            theta = (2 * M_PI) + theta;
+        }
+
+        std::cout << "Theta: " << theta << " rads // " << theta * (180 / M_PI) << " grados" << std::endl;
+        std::cout << "Angulo: " << angulo << " rads // " << angulo * (180 / M_PI)  <<" grados" << std::endl;
+
+        if (fabs(angulo - theta) < margen) {
+
+            // desplazamiento mejorado
+            // la velocidad depende de la distancia al goal
+
+
+            // distancia euclidea
+
+            double dx = gx - x;
+            double dy = gy - y;
+            double dist = sqrt((dx * dx) + (dy * dy));
+
+            // calculo de la velocidad
+
+            if (v_base * dist > v_base) {
+
+                linear_vel = v_base * dist;
+
+            } else {
+
+                linear_vel = v_base;
+
+            }
+
+            // corrección del angualo sobre la marcha
+            
+            if ((angulo - theta) > 0) {
+
+                angular_vel = 0.05;
+                std::cout << "Rotando IZQ sobre la marcha! " << std::endl;
+
+            } else {
+
+                angular_vel = -0.05;
+                std::cout << "Rotando DRA sobre la marcha! " << std::endl;
+
+            }
+
+            std::cout << "Distancia al objetivo: " << dist << std::endl;
+            std::cout << "Desplazando a  " << linear_vel << " m/s" << std::endl;
+
+        } else {
+
+            // rotacion mejorada
+            // gira en sentido del angulo menor
+
+
+            if ((angulo - theta) > 0) {
+
+                angular_vel = 0.1;
+                std::cout << "Rotando IZQ! " << std::endl;
+
+            } else {
+
+                angular_vel = -0.1;
+                std::cout << "Rotando DRA! " << std::endl;
+
+            }
+
+        }
+    }
+
+    publish(angular_vel, linear_vel);
+
 
     return ret_val;
 }
 
-/*
- *   findAltGoal finds a pair of alternatives coordenates when an 
- *   obstacle is detected 
- */
+bool Turtlebot::isClear(int k) {
 
-float Turtlebot::findAltGoal() {
-    
-    //if ( )
-    
-    
-    
-    return 0.0;
+    bool clear = true;
+    if (data_scan.ranges[k] <= 10)
+        clear = false;
+    return clear;
 }
 
+int Turtlebot::findClearRegion(int k) {
+
+    if (isClear(k + 1) == isClear(k + 2) && isClear(k) == isClear(k + 1) && isClear(k + 1) == true)
+        return 1;
+    else
+        return -1;
+}
+
+/*
+ *   findAltGoal finds a pair of alternatives coordenates when an
+ *   obstacle is detected
+ */
+
+std::vector<float> Turtlebot::findAltGoal() {
+
+    /*
+     *
+     *      Afecta a la trayectoria???
+     *
+     */
+
+
+    std::vector<float> vector(2);
+
+    //std::cout << "Angle min " << data_scan.angle_min << " rad" << ", " << data_scan.angle_min * (180/M_PI) <<" degrees" << std::endl;
+    //std::cout << "Angle max " << data_scan.angle_max << " rad" << ", " << data_scan.angle_max * (180/M_PI) <<" degrees" << std::endl;
+
+    // Get read #
+
+    bool encontrado = false;
+    bool obstcl_init; // true -> osbtacle; false -> clear
+    bool obstcl_read; // true -> osbtacle; false -> clear
+    int k = 0; // laserScan read number
+
+    obstcl_init = isClear(1);
+
+    std::cout << "isClear inicial: " << obstcl_init << std::endl;
+    std::cout << "Numero de lecturas : " << data_scan.ranges.size() << std::endl;
+
+    int i = 1;
+    bool drc;
+
+    while (encontrado == false && i < data_scan.ranges.size()) {
+
+        obstcl_read = isClear(i);
+        //std::cout << "Lectura #" << i << " Resultado: " << data_scan.ranges[i] << std::endl;
+        if (obstcl_init != obstcl_read) {
+
+            std::cout << "Posible lectura! " << std::endl;
+
+            bool read_plus1 = isClear(i + 1);
+            bool read_plus2 = isClear(i + 2);
+
+            if (read_plus1 == read_plus2 && read_plus1 == obstcl_read) {
+                
+                if (isClear(i)) {
+                    k = i - 1;
+                    drc = false; //clear region left
+                } else {
+                    k = i;
+                    drc = true; // clear region right
+                }
+                
+                encontrado = true;
+                std::cout << "Lectura encontrada: " << i << " isClear:  " << isClear(i) << std::endl;
+            }
+        }
+        i++;
+    }
+
+    /* for (int i = 1; i < data_scan.ranges.size(); i++){
+        //std::cout << "Lectura #" << i << " Resultado: " << data_scan.ranges[i] << std::endl;
+        obstcl_read = isClear(i);
+
+        if (obstcl_init != obstcl_read && encontrado == false){
+
+            std::cout << "Posible lectura! " << std::endl;
+
+            bool read_plus1 = isClear(i+1);
+            bool read_plus2 = isClear(i+2);
+
+            if (read_plus1 == read_plus2 && read_plus1 == obstcl_read) {
+                k = i;
+                encontrado = true;
+                std::cout << "Lectura encontrada: " << i << " isClear:  " << isClear(i) << std::endl;
+            }
+        }
+    } */
+
+    std::cout << "Lectura #" << k << " Salida: " << data_scan.ranges[k] << std::endl;
+
+    // Get angle
+
+    double gamma = data_scan.angle_min + (data_scan.angle_increment * k);
+
+    std::cout << "Angulo Gamma" << gamma << " rad" << ", " << gamma * (180 / M_PI) << " degrees" << std::endl;
+
+    // Get X point coordenates
+
+    double w = sin(gamma) * data_scan.ranges[k];
+    double h = cos(gamma) * data_scan.ranges[k];
+
+    std::cout << "R coordenates: " << x << ", " << y << std::endl;
+    std::cout << "(h, w): " << h << ", " << w << std::endl;
+    std::cout << "X coordenates: " << x + h << ", " << y + w << std::endl;
+
+    // Generate Alt Goal
+
+    double ax = x + h;
+    double ay;
+
+    if (!drc) {
+        ay = y + w + 1;
+        std::cout << "findClearRegion > 0: IZQ" << std::endl;
+
+    } else {
+        ay = y - w - 1;
+        std::cout << "findClearRegion < 0: DRC" << std::endl;
+    }
+
+
+    std::cout << "Alt Goal coordenates: " << ax << ", " << ay << std::endl;
+    //std::cout << "Alt Goal coordenates: " << vector << std::endl;
+
+    vector[0] = ax;
+    vector[1] = ay;
+
+    return vector;
+}
 
 /*
  *   isPathClear returns true if there is not any obstacle
@@ -127,11 +347,11 @@ bool Turtlebot::isPathClear() {
     bool isClear = true;
     float min = 10.0;
     int i = 0;
-    
-    while (i < data_scan.ranges.size() && isClear == true){
-        if (data_scan.ranges[i] < min) 
+
+    while (i < data_scan.ranges.size() && isClear == true) {
+        if (data_scan.ranges[i] < min)
             isClear = false;
-        i++;  
+        i++;
     }
     return isClear;
 }
@@ -167,84 +387,69 @@ void Turtlebot::receivePose(const nav_msgs::OdometryConstPtr& msg) {
 //Callback for robot position
 
 void Turtlebot::receiveKinect(const sensor_msgs::LaserScan& msg) {
-    
-    data_scan=msg;
-    /*
 
-    unsigned int num_readings = 100;
-    double laser_frequency = 40;
-    double ranges[num_readings];
-    double intensities[num_readings];
-
-    data_scan=msg;
-
-    std::cout << "\tRANGE: " << msg.ranges.size() << std::endl;
-
-    float min = 10.0;
-    float angular_vel = 0.0;
-    float linear_vel = 0.5;
-
-    for (unsigned int i = 0; i < msg.ranges.size(); i++ ){
-
-            std::cout << "\tRANGE: " << msg.ranges[i] << std::endl;
-
-            if (msg.ranges[i] < min ){
-                    min = msg.ranges[i];
-            }
-
-            if (min <= 2.0) {
-                    linear_vel = 0.0;
-                    angular_vel = 0.3;
-                    std::cout << "\tTurtlebot detenido !" << std::endl;
-                    std::cout << "\tDistancia al obstaculo: " << min << std::endl;
-            }
-    }
-     */
-    //publish(angular_vel,linear_vel);
+    data_scan = msg;
 }
+
 
 std::vector<geometry_msgs::Pose> loadPlan(const char *filename);
 void visualizePlan(const std::vector<geometry_msgs::Pose> &plan, ros::Publisher &marker_pub);
-
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "robot_control");
     Turtlebot robot;
     ros::NodeHandle n;
 
-    if (argc < 3) {
-        std::cout << "Insuficient number of parameters" << std::endl;
-        std::cout << "Usage: c1 Goal_coordinate_x Goal_coordinate_y" << std::endl;
+    if (argc < 2) {
+        std::cout << "Insufficient number of parameters" << std::endl;
+        std::cout << "Usage: robot_control <filename>" << std::endl;
         return 0;
     }
 
-    float xGoal = std::atof(argv[1]);
-    float yGoal = std::atof(argv[2]);
+    // vector
+    std::vector<geometry_msgs::Pose> plan = loadPlan(argv[1]);
+    unsigned int cont_wp = 0;
+
+    std::vector<float> alt_goal;
+    bool alt = false;
 
     ros::Rate loop_rate(20);
 
-    while (ros::ok()) {
+    while (ros::ok() && cont_wp < plan.size()) {
 
-		if (robot.isPathClear() == true){
+        ros::spinOnce();
 
-			std::cout << "Camino hacia el objetivo despejado!" << std::endl;
-			ros::spinOnce();
-			robot.command(xGoal, yGoal);
+        if (robot.isPathClear() == true && alt == false) {
 
-		} else {
+            std::cout << "Camino hacia el objetivo despejado!" << std::endl;
+            bool ret_value = robot.command(plan[cont_wp].position.x, plan[cont_wp].position.y);
 
-			std::cout << "El obstaculo obliga a cambiar la trayectoria!" << std::endl;
-			float altGoal = robot.findAltGoal();
+            if (ret_value == true) {
 
-			//std::cout << "Reconduciendo a las coordenadas: " << altGoal[0] << ", " << altGoal[1] << std::endl;
-			//robot.command(altGoal[0], altGoal[1]);
-		}
-		loop_rate.sleep();
-	}
+                cont_wp++;
+                std::cout << "\tGoal # " << cont_wp << " Reached !" << std::endl;
+            }
+
+        } else if (alt == true) {
+            std::cout << "Reconduciendo a las coordenadas: " << alt_goal[0] << ", " << alt_goal[1] << std::endl;
+            bool ret_alt = robot.command((double) alt_goal[0], (double) alt_goal[1]);
+
+            if (ret_alt == true)
+                alt = false;
+
+        } else {
+
+            std::cout << "El obstaculo obliga a cambiar la trayectoria!" << std::endl;
+            alt_goal = robot.findAltGoal();
+            alt = true;
+        }
+
+        std::cout << "------------------------------------------\n" << std::endl;
+        loop_rate.sleep();
+    }
 
     return 0;
 }
-
 
 std::vector<geometry_msgs::Pose> loadPlan(const char *filename) {
     std::vector<geometry_msgs::Pose> plan;
@@ -315,5 +520,3 @@ void visualizePlan(const std::vector< geometry_msgs::Pose >& plan, ros::Publishe
         marker_pub.publish(marker);
     }
 }
-
-
